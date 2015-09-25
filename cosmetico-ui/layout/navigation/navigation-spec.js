8ce2f4a431fea25f1eb8642cde7ffcf48bbf2fdb
Denis <den_2710@ukr.net>
2015-09-25 22:26:42 +0300

    added collapse to navbar, refactored styles structure for navigation

--- cosmetico-ui/layout/navigation/navigation-spec.js
+++ cosmetico-ui/layout/navigation/navigation-spec.js
@@ -39,3 +39,22 @@ describe('UserNavController', function() {
 	});
 
 });
+
+describe('NavbarController', function() {
+	beforeEach(module('cosmetico'));
+
+	var scope,ctrl;
+
+    beforeEach(inject(function($rootScope, $controller) {
+      scope = $rootScope.$new();
+      ctrl = $controller('NavbarController as navbar', {$scope: scope});
+    }));	
+
+	it('should instantiate scope', inject(function() {
+		expect(scope.navbar).toBeDefined();
+	}));
+
+	it('should set initial value of isCollapsed to true', inject(function() {
+		expect(scope.navbar.isCollapsed).toEqual(true);
+	}));
+});
